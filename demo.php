11f389039548c4be29285ebdc80637a5e75665dd
caoting <307298900@qq.com>
2021-01-22 20:51:46 +0800

    fixed

--- demo.php
+++ demo.php
@@ -2,9 +2,8 @@
 
 require "./vendor/autoload.php";
 
-use Cao\Plog\LogUtil;
+use Plog\LogUtil;
 
 $log = LogUtil::getLogFilePath('test', 'test', __DIR__);
 LogUtil::log2File($log, 'info');
 
-var_dump('aaa');
